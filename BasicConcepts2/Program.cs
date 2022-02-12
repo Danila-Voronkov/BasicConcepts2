a20957925e5369a1ac45bed51452bbd0ec660251
LAPTOP-95H61VHK\danil <danila.voronkov.2021@gmail.com>
2022-02-12 14:26:12 +0700

    Добавьте файлы проекта.

---
+++ BasicConcepts2/Program.cs
@@ -0,0 +1,29 @@
+﻿/*****************************************
+* Автор:    Воронков Д.А.                *
+* Название: Базовые понятия языка С# №2  *
+* Дата:     07.02.2022                   *
+*****************************************/
+
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      Console.Write("Введите число n : ");
+
+      string NumberN = Console.ReadLine();
+      char SecondSign = NumberN[1];
+
+      Console.WriteLine("Число x = " + NumberN.Remove(1, 1) + SecondSign);
+      Console.ReadKey();
+    }
+  }
+}
